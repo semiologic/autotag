2d77a7bd065ae13e1e9d4b5c3d6facce52f46e2f
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-02 11:44:03 +0000

    More references to mesoconcepts Ltd
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/autotag/trunk@468 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- extract-terms.php
+++ extract-terms.php
@@ -3,7 +3,7 @@
 Terms of use
 ------------
 
-This software is copyright Mesoconcepts Ltd, and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
 
 http://www.mesoconcepts.com/license/
 
@@ -17,7 +17,7 @@ Hat tips
 IMPORTANT
 ---------
 
-1. DO NOT USE THIS PLUGIN FOR COMMERCIAL PURPOSES or otherwise breach Yahoo!’s terms of use:
+1. DO NOT USE THIS PLUGIN FOR COMMERCIAL PURPOSES or otherwise breach Yahoo!Ã­s terms of use:
    http://developer.yahoo.net/terms/
 2. Note that your server's IP is eligible to 5,000 calls per 24h
    http://developer.yahoo.net/documentation/rate.html
