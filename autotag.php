faa8e8cbcd260eee630ccbdbd9383b7002c93257
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-21 10:03:10 +0000

    #289: use https directly -- automated plugin upgrades won't work before we set up amember anyway
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/autotag/trunk@576 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- autotag.php
+++ autotag.php
@@ -8,7 +8,7 @@ Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Service: http://version.semiologic.com/plugins
 Update Tag: autotag
-Update Package: http://members.semiologic.com/media/wp-plugins/autotag.zip
+Update Package: https://members.semiologic.com/media/wp-plugins/autotag.zip
 */
 
 /*
