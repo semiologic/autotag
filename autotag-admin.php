ecb67120d85fbe0f24af9afb812cbb8a1febb9c5
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-26 17:49:12 +0000

    new and improved autotag
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/autotag/trunk@1559 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- autotag-admin.php
+++ autotag-admin.php
@@ -1,240 +1,170 @@
 <?php
-class autotag
-{
-	#
-	# init()
-	#
+/**
+ * autotag_admin
+ *
+ * @package AutoTag
+ **/
 
-	function init()
-	{
-		add_action('admin_menu', array('autotag', 'add_meta_boxes'));
-		add_action('save_post', array('autotag', 'save_entry'));
-	} # init()
-	
-	
-	#
-	# add_meta_boxes()
-	#
-	
-	function add_meta_boxes()
-	{
-		add_meta_box('autotag', 'Autotag', array('autotag', 'entry_editor'), 'post', 'normal');
-	}
+add_action('save_post', array('autotag_admin', 'save_entry'));
 
+class autotag_admin {
+	/**
+	 * entry_editor()
+	 *
+	 * @param $post
+	 * @return void
+	 **/
 
-	#
-	# entry_editor()
-	#
-
-	function entry_editor()
-	{
+	function entry_editor($post) {
 		global $user_ID;
-		$post_ID = isset($GLOBALS['post_ID']) ? $GLOBALS['post_ID'] : $GLOBALS['temp_ID'];
-		
-		$published = false;
-		
-		if ( $post_ID > 0 )
-		{
-			$post =& get_post($post_ID);
-			$published = ( $post->post_status == 'publish' );
-		}
+		$post_ID = $post->ID;
+		$published = $post->post_status == 'publish';
 		
 		echo '<p>'
-			. __('Create tags automatically?')
+			. __('Create tags automatically?', 'autotag')
 			. '</p>' . "\n";
 		
-		if ( !$published
-			&& !( $post_ID > 0 && get_post_meta($post_ID, '_did_autotag', true) ) )
-		{
+		if ( !$published && !( $post_ID > 0 && get_post_meta($post_ID, '_did_autotag', true) ) ) {
 			if ( $post_ID > 0 && get_post_meta($post_ID, '_autotag', true)
 				|| $post_ID <= 0 && get_usermeta($user_ID, 'autotag')
-				)
-			{
+				) {
 				$default = 'publish';
-			}
-			else  
-			{
+			} else {
 				$default = '';
 			}
 			
 			echo '<p>'
 				. '<label>'
-				. '<input type="radio" name="sem_autotag" value=""'
+				. '<input type="radio" name="autotag" value=""'
 					. ( $default == ''
 						? ' checked="checked"'
 						: '' )
 					. ' />'
-				. '&nbsp;' . 'No, thanks!'
+				. '&nbsp;' . __('No. Do not autotag this entry.', 'autotag')
 				. '</label>' . '<br />' . "\n"
 				. '<label>'
-				. '<input type="radio" tabindex="4" name="sem_autotag" value="now" />'
-				. '&nbsp;' . 'Yes: Fetch some tags upon saving, I\'d like to edit the resulting list.'
+				. '<input type="radio" tabindex="4" name="autotag" value="now" />'
+				. '&nbsp;' . __('Yes. Fetch new tags immediately.', 'autotag')
 				. '</label>' . '<br />' . "\n"
 				. '<label>'
-				. '<input type="radio" tabindex="4" name="sem_autotag" value="publish"'
+				. '<input type="radio" tabindex="4" name="autotag" value="publish"'
 					. ( $default == 'publish'
 						? ' checked="checked"'
 						: '' )
 					. ' />'
-				. '&nbsp;' . 'Yes, but not yet: Do so upon publishing this entry.'
+				. '&nbsp;' . __('Yes. Fetch new tags upon publishing.', 'autotag')
 				. '</label>'
 				. '</p>' . "\n";
 				
-			echo '<input type="hidden" name="sem_autotag_sticky" value="1" />';
-		}
-		else
-		{
+			echo '<input type="hidden" name="autotag_sticky" value="1" />';
+		} else {
 			echo '<p>'
 				. '<label>'
-				. '<input type="radio" tabindex="4" name="sem_autotag" value="" checked="checked" />'
-				. '&nbsp;' . 'No, thanks!'
-					. ' '
-					. ( ( $post_ID > 0 && get_post_meta($post_ID, '_did_autotag', true) )
-						? 'I already did this.'
-						: 'This entry is published material.'
-						)
+				. '<input type="radio" tabindex="4" name="autotag" value="" checked="checked" />'
+				. '&nbsp;'
+				. ( ( $post_ID > 0 && get_post_meta($post_ID, '_did_autotag', true) )
+					? __('No. I did this already.', 'autotag')
+					: __('No. This entry has been published already.', 'autotag')
+					)
 				. '</label>' . '<br />' . "\n"
 				. '<label>'
-				. '<input type="radio" tabindex="4" name="sem_autotag" value="now" />'
-				. '&nbsp;' . 'Yes: Fetch some new tags.'
+				. '<input type="radio" tabindex="4" name="autotag" value="now" />'
+				. '&nbsp;' . __('Yes. Fetch new tags immediately.', 'autotag')
 				. '</label>'
 				. '</p>' . "\n";
 		}
 		
 		echo '<p>'
 			. '<label>'
-			. '<input type="checkbox" tabindex="4" name="sem_autotag_strip"'
+			. '<input type="checkbox" tabindex="4" name="autotag_strip"'
 				. ( get_usermeta($user_ID, 'autotag_strip')
 					? ' checked="checked"'
 					: ''
 					)
 				. ' />'
 				. '&nbsp;'
-				. 'Only keep terms that match existing tags'
+				. __('Only keep terms that match existing tags.', 'autotag')
 				. '</label>'
 				. '</p>' . "\n";
 		
 		echo '<p>'
-			. __('<b>Notice</b>: Autotag will only work provided your server\'s IP address hasn\'t hit Yahoo\'s web services 5,000 times in the past 24h. If it stops working, give it a new try the next day.')
-			. '</p>';
+			. __('<b>Notice</b>: AutoTag will only work provided your server\'s IP address hasn\'t hit Yahoo\'s web services 5,000 times in the past 24h. If it stops working, give it a new try the next day.', 'autotag')
+			. '</p>' . "\n";
 	} # entry_editor()
+	
+	
+	/**
+	 * save_entry()
+	 *
+	 * @param int $post_ID
+	 * @return void
+	 **/
 
-
-	#
-	# save_entry()
-	#
-
-	function save_entry($post_ID)
-	{
+	function save_entry($post_ID) {
 		$post = get_post($post_ID);
 		
-		if ( $post->post_type == 'revision' ) return;
+		if ( !in_array($post->post_type, array('post', 'page')) || !isset($_POST['autotag']) )
+			return;
 		
-		#dump($post_ID);
-		#return;
+		global $user_ID;
 		
-		if ( isset($_POST['sem_autotag']) )
-		{
-			global $user_ID;
-			
-			$fetch_terms = false;
-			$user_pref = false;
-			
-			$post = get_post($post_ID);
-			
-			switch ( $_POST['sem_autotag'])
-			{
-			case 'publish':
-				if ( in_array($post->post_status, array('publish', 'future')) )
-				{
-					$fetch_terms = true;
-					$user_pref = true;
-
-					delete_post_meta($post_ID, '_autotag');
-				}
-				else
-				{
-					$user_pref = true;
-
-					add_post_meta($post_ID, '_autotag', 1, true);
-				}
-				break;
-			
-			case 'now':
+		$fetch_terms = false;
+		$user_pref = false;
+		
+		$post = get_post($post_ID);
+		
+		switch ( isset($_POST['autotag']) ) {
+		case 'publish':
+			if ( in_array($post->post_status, array('publish', 'future')) ) {
 				$fetch_terms = true;
-			
-			default:
+				$user_pref = true;
+
 				delete_post_meta($post_ID, '_autotag');
-				$user_pref = false;
-				break;
+			} else {
+				$user_pref = true;
+				update_post_meta($post_ID, '_autotag', '1');
 			}
-			
-			if ( $fetch_terms )
-			{
-				$terms = extract_terms::get_post_terms($post);
+			break;
+		
+		case 'now':
+			$fetch_terms = true;
+		
+		default:
+			delete_post_meta($post_ID, '_autotag');
+			$user_pref = false;
+			break;
+		}
+		
+		if ( $fetch_terms ) {
+			load_yterms();
+			$terms = yterms::get($post, true);
 
-				if ( $terms )
-				{
-					if ( isset($_POST['sem_autotag_strip']) )
-					{
-						foreach ( $terms as $key => $term )
-						{
-							if ( !is_term($term, 'post_tag') )
-							{
-								unset($terms[$key]);
-							}
-						}
+			if ( $terms ) {
+				if ( isset($_POST['autotag_strip']) ) {
+					foreach ( $terms as $key => $term ) {
+						if ( !is_term($term, 'post_tag') )
+							unset($terms[$key]);
 					}
-					
-					if ( count($terms) > 3 )
-					{
-						$terms = array_slice($terms, 0, 3 + round(log(count($terms))));
-				 	}
-
-					$tags = '';
-
-					foreach ( $terms as $term )
-					{
-						$tags .= ( $tags ? ', ' : '' ) . $term;
-					}
-
-					wp_set_post_tags($post_ID, $tags, true);
-					
-					add_post_meta($post_ID, '_did_autotag', 1, true);
-				}
-			}
-			
-			if ( $_POST['sem_autotag_sticky'] )
-			{
-				if ( $user_pref )
-				{
-					if ( !( $user_prefs = get_usermeta($user_ID, 'autotag') ) )
-					{
-						update_usermeta($user_ID, 'autotag', '1');
-					}
-				}
-				else
-				{
-					update_usermeta($user_ID, 'autotag', '0');
 				}
 				
-				if ( isset($_POST['sem_autotag_strip']) )
-				{
-					update_usermeta($user_ID, 'autotag_strip', '1');
-				}
-				else
-				{
-					update_usermeta($user_ID, 'autotag_strip', '0');
-				}
+				wp_set_post_tags($post_ID, $terms, true);
 			}
+			
+			update_post_meta($post_ID, '_did_autotag', '1');
+		}
+		
+		if ( isset($_POST['autotag_sticky']) ) {
+			if ( $user_pref ) {
+				update_usermeta($user_ID, 'autotag', '1');
+			} else {
+				update_usermeta($user_ID, 'autotag', '0');
+			}
+			
+			if ( isset($_POST['autotag_strip']) )
+				update_usermeta($user_ID, 'autotag_strip', '1');
+			else
+				update_usermeta($user_ID, 'autotag_strip', '0');
 		}
 	} # save_entry()
-} # end autotag
-
-autotag::init();
-
-if ( !class_exists('extract_terms') )
-{
-	include dirname(__FILE__) . '/extract-terms.php';
-}
+} # autotag_admin
 ?>
--- autotag.php
+++ autotag.php
@@ -3,7 +3,7 @@
 Plugin Name: AutoTag
 Plugin URI: http://www.semiologic.com/software/autotag/
 Description: Leverages Yahoo!'s term extraction web service to automatically tag your posts.
-Version: 2.2 alpha
+Version: 3 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: autotag-info
@@ -20,8 +20,41 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-if ( is_admin() )
-{
+load_plugin_textdomain('autotag', null, dirname(__FILE__) . '/lang');
+
+
+/**
+ * autotag
+ *
+ * @package AutoTag
+ **/
+
+add_action('admin_menu', array('autotag', 'meta_boxes'));
+
+class autotag {
+	/**
+	 * meta_boxes()
+	 *
+	 * @return void
+	 **/
+
+	function meta_boxes() {
+		add_meta_box('autotag', __('AutoTag', 'autotag'), array('autotag_admin', 'entry_editor'), 'post', 'normal');
+	} # meta_boxes()
+} # autotag
+
+
+function load_autotag_admin() {
 	include dirname(__FILE__) . '/autotag-admin.php';
 }
+
+foreach ( array('post.php', 'post-new.php', 'page.php', 'page-new.php') as $hook )
+	add_action("load-$hook", 'load_autotag_admin');
+
+if ( !function_exists('load_yterms') ) :
+function load_yterms() {
+	if ( !class_exists('yterms') )
+		include dirname(__FILE__) . '/yterms/yterms.php';
+}
+endif;
 ?>
