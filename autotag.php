e3ecf06fc3ce6b0cffb9c72d69c2e3aaa88115ad
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-10 16:12:24 +0000

    misc: fix incorrect load_plugin_textdomain calls
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/autotag/trunk@2047 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- autotag.php
+++ autotag.php
@@ -20,7 +20,7 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-load_plugin_textdomain('autotag', null, dirname(__FILE__) . '/lang');
+load_plugin_textdomain('autotag', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 
 /**
